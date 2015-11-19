71644ad781e390a6d6a332588905a9d61af2ebd5
= <ryanchenkie@gmail.com>
2015-11-18 20:06:40 -0700

    First commit

---
+++ readme.md
@@ -0,0 +1,14 @@
+# Vue.js Example - Display GitHub Users
+
+## Setup
+
+Install dependencies and run the app.
+
+```bash
+npm install
+npm run dev
+```
+
+The app will be served at `localhost:8080`.
+
+
---
+++ src/components/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="navbar navbar-default">
+    <div class="container">
+      <ul class="nav navbar-nav">
+        <li><a v-link="'home'">Home</a></li>
+        <li><a v-link="'users'">Users</a></li>
+      </ul>
+    </div>    
+  </nav>
+  <div class="container">
+    <router-view></router-view>
+  </div>
+</template>
