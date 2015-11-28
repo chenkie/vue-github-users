c4ab3535558e8d6f4faa8495848808f5c9a09f4f
= <ryanchenkie@gmail.com>
2015-11-28 14:47:58 -0700

    put in just one root element

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <nav class="navbar navbar-default">
+  <div id="wrapper">
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <ul class="nav navbar-nav">
+          <li><a v-link="'home'">Home</a></li>
+          <li><a v-link="'users'">Users</a></li>
+        </ul>
+      </div>    
+    </nav>
     <div class="container">
-      <ul class="nav navbar-nav">
-        <li><a v-link="'home'">Home</a></li>
-        <li><a v-link="'users'">Users</a></li>
-      </ul>
-    </div>    
-  </nav>
-  <div class="container">
-    <router-view></router-view>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
